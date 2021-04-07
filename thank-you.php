608a251bd920cbaf4869387a8678954366ffa559
akarenpadron <anna.navarro@uabc.edu.mx>
2021-04-07 08:35:57 -0700

    prueba

---
+++ thank-you.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139306737-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-139306737-1');
+  </script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Score Sports</title>
+<link href="css/main.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript">
+var iWebkit;if(!iWebkit){iWebkit=window.onload=function(){function fullscreen(){var a=document.getElementsByTagName("a");for(var i=0;i<a.length;i++){if(a[i].className.match("noeffect")){}else{a[i].onclick=function(){window.location=this.getAttribute("href");return false}}}}function hideURLbar(){window.scrollTo(0,0.9)}iWebkit.init=function(){fullscreen();hideURLbar()};iWebkit.init()}}
+</script>
+<meta name="apple-mobile-web-app-capable" content="yes" />
+</head>
+<style>
+
+
+img{
+  margin-top:30px;
+  max-width:250px;
+}
+
+h3,h2,h1{
+  margin:50px;
+}
+
+.thankyou{
+  background-image: url("img/thankyou-back.png");
+  background-size: contain, cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+</style>
+<body>
+  <div class="container thankyou">
+    <div class="row">
+      <div class="col-12"><center>
+<a href="index.html"><img src="img/logo-ss.png" alt="Score Sports" width="30%" border="0" /></a>
+<h2>Your request has been submitted.</h2>
+
+<h1>Thank You!</h1>
+
+<h3>A Score® Representative will be in touch with you within the next 24-48 hrs.</h3>
+<a href="index.html" class="genric-btn primary radius"> GO TO HOME</a></center>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</body>
+</html>
