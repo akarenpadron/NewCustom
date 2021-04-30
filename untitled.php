f58d0e335dd4e3c23903078dbb1c1dac8cc894d2
akarenpadron <anna.navarro@uabc.edu.mx>
2021-04-30 16:56:13 -0700

    Logo and design colors on local storage

---
+++ untitled.php
@@ -0,0 +1,41 @@
+<script src="js/vendor/jquery-2.2.4.min.js"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139383456-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-139383456-1');
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+ crossorigin="anonymous"></script>
+<script src="js/tilt.jquery.min.js"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+<script src="js/easing.min.js"></script>
+<script src="js/hoverIntent.js"></script>
+<script src="js/superfish.min.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/owl-carousel-thumb.min.js"></script>
+<script src="js/hexagons.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/mail-script.js"></script>
+<script src="js/main.js"></script>
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="https://kit.fontawesome.com/a6517f657d.js" crossorigin="anonymous"></script>
+<script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+<script type='text/javascript' src='scripts/fg_captcha_validator.js'></script>
+<script type="text/javascript">
+  var iWebkit;if(!iWebkit){iWebkit=window.onload=function(){function fullscreen(){var a=document.getElementsByTagName("a");for(var i=0;i<a.length;i++){if(a[i].className.match("noeffect")){}else{a[i].onclick=function(){window.location=this.getAttribute("href");return false}}}}function hideURLbar(){window.scrollTo(0,0.9)}iWebkit.init=function(){fullscreen();hideURLbar()};iWebkit.init()}}
+  function MM_showHideLayers() { //v9.0
+    var i,p,v,obj,args=MM_showHideLayers.arguments;
+    for (i=0; i<(args.length-2); i+=3) with (document) if (getElementById && ((obj=getElementById(args[i]))!=null)) { 
+      v=args[i+2];
+      if (obj.style) { obj=obj.style; v=(v=='show')?'visible':(v=='hide')?'hidden':v; }
+      obj.visibility=v; 
+    }
+  }
+</script>
+<script src="js/contact.js"></script>
